fd489db2b384e22f32fd014318253b9786f856e6
Meghana0804 <120400517+Meghana0804@users.noreply.github.com>
2023-02-01 19:08:32 +0530

    updating the file

--- file.py
+++ file.py
@@ -5,11 +5,10 @@ Created on Mon Jun  1 19:13:36 2020
 @author: bvv
 """
 
-
-
 from flask import Flask, request, jsonify, render_template
 import pickle
 import numpy as np
+
 
 app = Flask(__name__)
 model = pickle.load(open('final.pkl', 'rb'))
@@ -25,26 +24,26 @@ def predict():
     '''
     For rendering results on HTML GUI
     '''
-    int_features = [int(x) for x in request.form.values()]
-    print(int_features)
-    final_features = [np.array(int_features)]
-    print(final_features)
-    prediction = model.predict(final_features)
+    int_ft = [int(x) for x in request.form.values()]
+    print(int_ft)
+    final_ft = [np.array(int_ft)]
+    print(final_ft)
+    prediction = model.predict(final_ft)
     #print(prediction)
-    output = round(prediction[0], 2)
+    final_values = round(prediction[0], 2)
 
-    return render_template('index.html', prediction_text='Rating is  {}'.format(output))
+    return render_template('index.html', prediction_text='Rating is  {}'.format(final_values))
 
 @app.route('/predict_api',methods=['POST'])
 def predict_api():
     '''
     For direct API calls trought request
     '''
-    data = request.get_json(force=True)
-    prediction = model.predict([np.array(list(data.values()))])
+    data_values = request.get_json(force=True)
+    prediction = model.predict([np.array(list(data_values.values()))])
 
-    output = prediction[0]
-    return jsonify(output)
+    final_values = prediction[0]
+    return jsonify(final_values)
 
 if __name__ == "__main__":
     app.run(debug=False)
